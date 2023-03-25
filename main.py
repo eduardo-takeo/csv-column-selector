b44811a80ae714f8c1a7605ae0c4d1744e733725
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2023-03-25 15:15:18 -0300

    add function to normalize csv columns names

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# main.py
+import pandas as pd
+
+dataframe = pd.read_csv("csv/cities.csv")
+
+def normalizeColumn(column):
+    return column.strip('"" ')
+
+normalizedColumns = list(map(normalizeColumn, dataframe.columns.tolist()))
+print(normalizedColumns)
